98308cdfbd66c3590bfec7de42fd6734d9b688d9
Leon Schwend <50238981+lschwend@users.noreply.github.com>
2024-01-06 12:42:57 +0100

    create spring boot application

---
+++ Backend/timerecording/src/main/java/com/github/lschwend/timerecording/timerecording/TimerecordingApplication.java
@@ -0,0 +1,13 @@
+package com.github.lschwend.timerecording.timerecording;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimerecordingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimerecordingApplication.class, args);
+	}
+
+}
---
+++ Backend/timerecording/src/test/java/com/github/lschwend/timerecording/timerecording/TimerecordingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lschwend.timerecording.timerecording;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimerecordingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
